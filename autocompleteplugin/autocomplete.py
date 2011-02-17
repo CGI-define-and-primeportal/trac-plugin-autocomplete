9b26b939a4eb2ef61ffc95808ac5101050dc8f06
Pontus Enmark <pontus.enmark@cgi.com>
2011-02-17 16:47:55 +0000

    Add stripes and autocomplete to batch modify form. See #652 Took 2 hours
    git-svn-id: file:///mnt/ramdisk/d4-svn/branches/features/redesign-3@2391 484df14f-75d2-4b3f-8ba0-490e05069a01

--- autocompleteplugin/autocomplete.py
+++ autocompleteplugin/autocomplete.py
@@ -110,9 +110,9 @@ class AutoCompleteForTickets(Component):
                                 ('#field-keywords', 'text', '{source: %s}' % to_json(
                         self._current_keywords).encode('utf8'))],
                 # These are for fields that are pre-loaded using a stored query for instance
-                "query.html": [('input[name$=_owner]', 'select'),
-                               ('input[name$=_reporter]', 'select'),
-                               ('input[name$=_qualityassurancecontact]', 'select')],
+                "query.html": [('#filters input[name$=_owner]', 'select'),
+                               ('#filters input[name$=_reporter]', 'select'),
+                               ('#filters input[name$=_qualityassurancecontact]', 'select')],
                 "admin_components.html": [("input[name='owner']", 'select')]}
 
     # ITicketChangeListener
